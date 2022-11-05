77b8c9201f225e03dc9d89d3c2c9bd564db7fcc9
Mert-coderoid <nevi.elric@gmail.com>
2022-11-05 19:04:36 +0300

    Ders 4

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace console_programing_csharp
         */
         public static void Main(string[] args)
         {   
-            // ##################### DERS 1 #####################
+// ##################### DERS 1 #####################
             // - "Console.WriteLine" ifadesi ekrana yazı yazdırmak için kullanılır.
             // - "Console.ReadLine" ifadesi kullanıcıdan veri almak için kullanılır.
             /* 
@@ -30,7 +30,7 @@ namespace console_programing_csharp
 
             */
 
-            // ##################### DERS 2 #####################
+// ##################### DERS 2 #####################
 
             // - "byte" ifadesi 8 bitlik yani 1 byte'lık bir tamsayı tipidir. 0 ile 255 arasında değer alır.
             // - "sbyte" ifadesi 8 bitlik yani 1 byte'lık bir tamsayı tipidir. -128 ile 127 arasında değer alır.
@@ -155,13 +155,53 @@ namespace console_programing_csharp
             string datetime6 = DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss");
             Console.WriteLine(datetime6); // output: 01-01-2021 00:00:00
 
-            
+            // - "Parse" ifadesi değişken dönüşümü yapmak için kullanılır.
 
-            
+            string number28 = "10";
+            int number29 = int.Parse(number28);
+            Console.WriteLine(number29 + 5);
 
-            
+            string number30 = "10.25";
+            double number31 = double.Parse(number30);
 
+            // - "TryParse" ifadesi değişken dönüşümü yapmak için kullanılır.
 
+            string number32 = "10";
+            int number33;
+            bool result = int.TryParse(number32, out number33);
+            Console.WriteLine(number33 + 5);
+
+            string number34 = "10.25";
+            double number35;
+            bool result2 = double.TryParse(number34, out number35);
+
+            // - "ToString" ifadesi değişken dönüşümü yapmak için kullanılır.
+
+            int number36 = 10;
+            string number37 = number36.ToString();
+            Console.WriteLine(number37 + 5);
+
+            double number38 = 10.25;
+            string number39 = number38.ToString();
+
+            // - "GetType" ifadesi değişkenin tipini öğrenmek için kullanılır.
+
+            int number40 = 10;
+            Console.WriteLine(number40.GetType());
+
+            double number41 = 10.25;
+            Console.WriteLine(number41.GetType());
+
+            string character9 = "A";
+            Console.WriteLine(character9.GetType());
+
+            // - "GetTypeCode" ifadesi değişkenin tipini öğrenmek için kullanılır.
+
+            int number42 = 10;
+            Console.WriteLine(number42.GetTypeCode());
+
+            double number43 = 10.25;
+            Console.WriteLine(number43.GetTypeCode());
 
             // - "int" ifadesi integer türünden bir değişken tanımlamak için kullanılır.
             int deger;
@@ -188,6 +228,224 @@ namespace console_programing_csharp
             Console.WriteLine("The number you entered is: " + (deger + 5) );
             Console.WriteLine("Press any key to exit...");
             Console.ReadLine();
+
+
+// ##################### DERS 3 #####################
+            int x = 10;
+            int y = 20;
+            int z = 30;
+
+            // - "++" ifadesi değişkenin değerini 1 arttırmak için kullanılır.
+
+            x++;
+            Console.WriteLine(x); // output: 11
+
+            // - "--" ifadesi değişkenin değerini 1 azaltmak için kullanılır.
+
+            y--;
+            Console.WriteLine(y); // output: 19
+
+            // - "+=" ifadesi değişkenin değerini belirtilen sayı kadar arttırmak için kullanılır.
+
+            z += 5;
+            Console.WriteLine(z); // output: 35
+
+            // - "-=" ifadesi değişkenin değerini belirtilen sayı kadar azaltmak için kullanılır.
+
+            z -= 5;
+            Console.WriteLine(z); // output: 30
+
+            // - "*=" ifadesi değişkenin değerini belirtilen sayı ile çarpmak için kullanılır.
+
+            z *= 5;
+            Console.WriteLine(z); // output: 150
+
+            // - "/=" ifadesi değişkenin değerini belirtilen sayıya bölerek atamak için kullanılır.
+
+            z /= 5;
+            Console.WriteLine(z); // output: 30
+
+            // - "%=" ifadesi değişkenin değerini belirtilen sayıya bölerek kalanı atamak için kullanılır.
+
+            z %= 5;
+            Console.WriteLine(z); // output: 0
+
+            // - "==" ifadesi değişkenin değerinin eşit olup olmadığını kontrol etmek için kullanılır.
+
+            int a = 10;
+            int b = 20;
+            Console.WriteLine(a == b); // output: False
+
+            // - "!=" ifadesi değişkenin değerinin eşit olmadığını kontrol etmek için kullanılır.
+
+            Console.WriteLine(a != b); // output: True
+
+            // - ">" ifadesi değişkenin değerinin belirtilen sayıdan büyük olup olmadığını kontrol etmek için kullanılır.
+
+            Console.WriteLine(a > b); // output: False
+
+            // - "<" ifadesi değişkenin değerinin belirtilen sayıdan küçük olup olmadığını kontrol etmek için kullanılır.
+
+            Console.WriteLine(a < b); // output: True
+
+            // - ">=" ifadesi değişkenin değerinin belirtilen sayıdan büyük veya eşit olup olmadığını kontrol etmek için kullanılır.
+
+            Console.WriteLine(a >= b); // output: False
+
+            // - "<=" ifadesi değişkenin değerinin belirtilen sayıdan küçük veya eşit olup olmadığını kontrol etmek için kullanılır.
+
+            Console.WriteLine(a <= b); // output: True
+
+            // - "&&" ifadesi değişkenin değerinin belirtilen sayıdan büyük ve küçük olup olmadığını kontrol etmek için kullanılır.
+
+            Console.WriteLine(a > 5 && a < 15); // output: True
+
+            // - "||" ifadesi değişkenin değerinin belirtilen sayıdan büyük veya küçük olup olmadığını kontrol etmek için kullanılır.
+
+            Console.WriteLine(a > 5 || a < 15); // output: True
+
+            // - "!" ifadesi değişkenin değerinin belirtilen sayıdan büyük veya küçük olup olmadığını kontrol etmek için kullanılır.
+
+            Console.WriteLine(!(a > 5 || a < 15)); // output: False
+
+            // - "if" ifadesi değişkenin değerinin belirtilen sayıdan büyük veya küçük olup olmadığını kontrol etmek için kullanılır.
+
+            if (a > 5)
+            {
+                Console.WriteLine("a is greater than 5");
+            }
+            else if (a < 5)
+            {
+                Console.WriteLine("a is less than 5");
+            }
+            else
+            {
+                Console.WriteLine("a is equal to 5");
+            }
+
+            // - "switch" ifadesi değişkenin değerinin belirtilen sayıdan büyük veya küçük olup olmadığını kontrol etmek için kullanılır.
+
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("a is equal to 1");
+                    break;
+                case 2:
+                    Console.WriteLine("a is equal to 2");
+                    break;
+                case 3:
+                    Console.WriteLine("a is equal to 3");
+                    break;
+                default:
+                    Console.WriteLine("a is not equal to 1, 2 or 3");
+                    break;
+            }
+
+            // - "while" ifadesi değişkenin değerinin belirtilen sayıdan büyük veya küçük olup olmadığını kontrol etmek için kullanılır.
+
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            // - "do while" ifadesi değişkenin değerinin belirtilen sayıdan büyük veya küçük olup olmadığını kontrol etmek için kullanılır.
+
+            int j = 0;
+            do
+            {
+                Console.WriteLine(j);
+                j++;
+            } while (j < 10);
+
+            // - "for" ifadesi değişkenin değerinin belirtilen sayıdan büyük veya küçük olup olmadığını kontrol etmek için kullanılır.
+
+            for (int k = 0; k < 10; k++)
+            {
+                Console.WriteLine(k);
+            }
+
+            // - "foreach" ifadesi değişkenin değerinin belirtilen sayıdan büyük veya küçük olup olmadığını kontrol etmek için kullanılır.
+
+            string[] names = { "John", "Jane", "Mary" };
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            // - "break" ifadesi değişkenin değerinin belirtilen sayıdan büyük veya küçük olup olmadığını kontrol etmek için kullanılır.
+
+            for (int k = 0; k < 10; k++)
+            {
+                if (k == 5)
+                {
+                    break;
+                }
+                Console.WriteLine(k);
+            }
+
+            // - "continue" ifadesi değişkenin değerinin belirtilen sayıdan büyük veya küçük olup olmadığını kontrol etmek için kullanılır.
+
+            for (int k = 0; k < 10; k++)
+            {
+                if (k == 5)
+                {
+                    continue;
+                }
+                Console.WriteLine(k);
+            }
+
+
+            // - "goto" ifadesi değişkenin değerinin belirtilen sayıdan büyük veya küçük olup olmadığını kontrol etmek için kullanılır.
+
+            for (int k = 0; k < 10; k++)
+            {
+                if (k == 5)
+                {
+                    goto end;
+                }
+                Console.WriteLine(k);
+            }
+            end:
+            Console.WriteLine("end");
+
+
+            // - "try catch" ifadesi değişkenin değerinin belirtilen sayıdan büyük veya küçük olup olmadığını kontrol etmek için kullanılır.
+
+            try
+            {
+                int[] numbers = { 1, 2, 3 };
+                Console.WriteLine(numbers[10]);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error occured");
+            }
+            finally
+            {
+                Console.WriteLine("The 'try catch' is finished");
+            }
+
+            // - "throw" ifadesi değişkenin değerinin belirtilen sayıdan büyük veya küçük olup olmadığını kontrol etmek için kullanılır.
+
+            int age = 15;
+            if (age < 18)
+            {
+                throw new Exception("Access denied - You must be at least 18 years old.");
+            }
+            else
+            {
+                Console.WriteLine("Access granted - You are old enough!");
+            }
+
+            // - "return" ifadesi değişkenin değerinin belirtilen sayıdan büyük veya küçük olup olmadığını kontrol etmek için kullanılır.
+
+            int sum(int x, int y)
+            {
+                return x + y;
+            }
+            Console.WriteLine(sum(5, 3));    
         }
     }
 }
